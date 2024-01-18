15bcecbc32c944ba41a0c5940851912919ad3ca1
caaano <wlsrl0515@naver.com>
2024-01-18 10:56:21 +0900

    데이터 검증

---
+++ src/main/java/com/myblog/controller/PostController.java
@@ -0,0 +1,21 @@
+package com.myblog.controller;
+
+import com.myblog.request.PostCreate;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@Slf4j
+@RestController
+public class PostController {
+
+    @PostMapping ("/posts")
+    public Map<String, String> post(@RequestBody @Valid PostCreate params) {
+
+        return Map.of();
+    }
+}
