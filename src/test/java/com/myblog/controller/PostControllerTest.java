dc75b4d5248cead3aaf42ce65ef36397e9800c39
caaano <wlsrl0515@naver.com>
2024-01-23 02:33:08 +0900

    페이징 처리

--- src/test/java/com/myblog/controller/PostControllerTest.java
+++ src/test/java/com/myblog/controller/PostControllerTest.java
@@ -12,6 +12,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
@@ -136,29 +140,25 @@ class PostControllerTest {
     @DisplayName("글 여러개 조회")
     void test5() throws Exception {
         // given
-        Post post1 = postRepository.save(Post.builder()
-                .title("title_1")
-                .content("content_1")
-                .build());
+        List<Post> requestPosts = IntStream.range(1, 31)
+                .mapToObj(i -> {
+                    return Post.builder()
+                            .title("myblog 제목 " + i)
+                            .content("반포자이 " + i)
+                            .build();
+                })
+                .collect(Collectors.toList());
 
-
-        Post post2 = postRepository.save(Post.builder()
-                .title("title_2")
-                .content("content_2")
-                .build());
+        postRepository.saveAll(requestPosts);
 
         // expected (when + then)
-        mockMvc.perform(get("/posts")
-                        .contentType(APPLICATION_JSON)
-                )
+        mockMvc.perform(get("/posts?page=1&sort=id,desc")
+                        .contentType(APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.length()", is(2)))
-                .andExpect(jsonPath("$.[0].id").value(post1.getId()))
-                .andExpect(jsonPath("$.[0].title").value("title_1"))
-                .andExpect(jsonPath("$.[0].content").value("content_1"))
-                .andExpect(jsonPath("$.[1].id").value(post2.getId()))
-                .andExpect(jsonPath("$.[1].title").value("title_2"))
-                .andExpect(jsonPath("$.[1].content").value("content_2"))
+                .andExpect(jsonPath("$.length()", is(5)))
+                .andExpect(jsonPath("$[0].id").value(30))
+                .andExpect(jsonPath("$[0].title").value("myblog 제목 30"))
+                .andExpect(jsonPath("$[0].content").value("반포자이 30"))
                 .andDo(print());
     }
 }
