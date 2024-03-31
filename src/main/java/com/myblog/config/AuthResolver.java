c78918f0c7206cc6c46c26e83181c54c4fd295c0
caaano <wlsrl0515@naver.com>
2024-03-31 23:47:50 +0900

    쿠키를 통한 인증 및 검증

--- src/main/java/com/myblog/config/AuthResolver.java
+++ src/main/java/com/myblog/config/AuthResolver.java
@@ -4,13 +4,17 @@ import com.myblog.config.data.UserSession;
 import com.myblog.domain.Session;
 import com.myblog.exception.Unauthorized;
 import com.myblog.repository.SessionRepository;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+@Slf4j
 @RequiredArgsConstructor
 public class AuthResolver implements HandlerMethodArgumentResolver {
 
@@ -23,14 +27,22 @@ public class AuthResolver implements HandlerMethodArgumentResolver {
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        String accessToken = webRequest.getHeader("Authorization");
-        if (accessToken == null || accessToken.equals("")) {
+        HttpServletRequest servletRequest = webRequest.getNativeRequest(HttpServletRequest.class);
+        if (servletRequest == null) {
+            log.error("serveletRequest null");
             throw new Unauthorized();
         }
 
+        Cookie[] cookies = servletRequest.getCookies();
+        if (cookies.length == 0) {
+            log.error("쿠키가 없음");
+            throw new Unauthorized();
+        }
+
+        String accessToken = cookies[0].getValue();
+
         Session session = sessionRepository.findByAccessToken(accessToken)
                 .orElseThrow(Unauthorized::new);
-
 
         return new UserSession(session.getUser().getId());
     }
