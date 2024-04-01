44446140e54d428dc56e5a173d54192f699deab8
caaano <wlsrl0515@naver.com>
2024-04-01 21:38:51 +0900

    JWT를 이용한 인증

--- src/main/java/com/myblog/config/AuthResolver.java
+++ src/main/java/com/myblog/config/AuthResolver.java
@@ -1,13 +1,15 @@
 package com.myblog.config;
 
 import com.myblog.config.data.UserSession;
-import com.myblog.domain.Session;
 import com.myblog.exception.Unauthorized;
 import com.myblog.repository.SessionRepository;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
@@ -18,6 +20,7 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 @RequiredArgsConstructor
 public class AuthResolver implements HandlerMethodArgumentResolver {
 
+    private static final String KEY = "bVsMNSmHS7ZNBdtBrdH6iO4BAZdPMKgA5O3zNNi4+Bg=";
     private final SessionRepository sessionRepository;
 
     @Override
@@ -27,23 +30,22 @@ public class AuthResolver implements HandlerMethodArgumentResolver {
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        HttpServletRequest servletRequest = webRequest.getNativeRequest(HttpServletRequest.class);
-        if (servletRequest == null) {
-            log.error("serveletRequest null");
+        String jws = webRequest.getHeader("Authorization");
+        if (jws == null || jws.equals("")) {
             throw new Unauthorized();
         }
 
-        Cookie[] cookies = servletRequest.getCookies();
-        if (cookies.length == 0) {
-            log.error("쿠키가 없음");
+        byte[] decodedKey = Base64.decodeBase64(KEY);
+
+        try {
+            Jws<Claims> claims = Jwts.parser()
+                    .setSigningKey(decodedKey)
+                    .build()
+                    .parseSignedClaims(jws);
+            String userId = claims.getBody().getSubject();
+            return new UserSession(Long.parseLong(userId));
+        } catch (JwtException e) {
             throw new Unauthorized();
         }
-
-        String accessToken = cookies[0].getValue();
-
-        Session session = sessionRepository.findByAccessToken(accessToken)
-                .orElseThrow(Unauthorized::new);
-
-        return new UserSession(session.getUser().getId());
     }
 }
